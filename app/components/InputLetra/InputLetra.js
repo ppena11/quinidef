4df144b67f052a79a515ce8832a1aa728cc50610
Pedro Pena <ppena.it@gmail.com>
2018-02-20 20:44:00 -0500

    working

--- app/components/InputLetra/InputLetra.js
+++ app/components/InputLetra/InputLetra.js
@@ -5,18 +5,17 @@ import styles from './styles';
 import color from '../../comun/colors';
 
 class InputLetra extends Component {
-
   render() {
-    return(
+    return (
       <View style={styles.container}>
         <TextInput
           style={styles.input}
-          placeholderTextColor = {color.$placeholderTextColor}
-          underlineColorAndroid = {color.$underlineColorAndroid}
-          textAlign = 'center'
-          autoCapitalize = 'characters'
-          placeholder = 'X'
-          maxLength = {1}
+          placeholderTextColor={color.$placeholderTextColor}
+          underlineColorAndroid={color.$underlineColorAndroid}
+          textAlign="center"
+          autoCapitalize="characters"
+          placeholder="X"
+          maxLength={1}
         />
       </View>
     );
--- app/config/routes.js
+++ app/config/routes.js
@@ -14,40 +14,41 @@ import UnirseAQuiniela from '../screens/UnirseAQuiniela';
 
 const stackRouterConfig = { initialRouteName: 'Home' };
 
-const RootNavigator = StackNavigator({
-  Home: {
-    screen: Login,
-  },
-  ReiniciarContrasena: {
-    screen: ReiniciarContrasena,
-  },
-  CrearCuenta: {
-    screen: CrearCuenta,
-  },
-  ConfirmacionCorreo: {
-    screen: ConfirmacionCorreo,
-  },
-  Log: {
-    screen: Home,
-  },
-  TusQuinielas: {
-    screen: TusQuinielas,
-  },
-  QuinielasAdministradas: {
-    screen: QuinielasAdministradas,
-  },
-  CreaciondeQuiniela: {
-    screen: CreaciondeQuiniela,
-  },
+const RootNavigator = StackNavigator(
+  {
+    Home: {
+      screen: Login,
+    },
+    ReiniciarContrasena: {
+      screen: ReiniciarContrasena,
+    },
+    CrearCuenta: {
+      screen: CrearCuenta,
+    },
+    ConfirmacionCorreo: {
+      screen: ConfirmacionCorreo,
+    },
+    Log: {
+      screen: Home,
+    },
+    TusQuinielas: {
+      screen: TusQuinielas,
+    },
+    QuinielasAdministradas: {
+      screen: QuinielasAdministradas,
+    },
+    CreaciondeQuiniela: {
+      screen: CreaciondeQuiniela,
+    },
 
-  DetalleQuinielaAdministrada: {
-    screen: DetalleQuinielaAdministrada,
-
+    DetalleQuinielaAdministrada: {
+      screen: DetalleQuinielaAdministrada,
+    },
     UnirseAQuiniela: {
       screen: UnirseAQuiniela,
     },
   },
   stackRouterConfig,
-});
+);
 
 export default RootNavigator;
