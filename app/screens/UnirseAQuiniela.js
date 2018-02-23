8f9f59f8b5d282a29a1f2c7ca35c9a935f5026f2
Francisco Díaz <frandiaz+git@gmail.com>
2018-02-23 13:12:14 -0500

    funcion para generar codigo

--- app/screens/UnirseAQuiniela.js
+++ app/screens/UnirseAQuiniela.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StatusBar, View, Text } from 'react-native';
+import { StatusBar, View, Text, BackHandler } from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import { connect } from 'react-redux';
 //import firebase from 'firebase';
@@ -15,12 +15,37 @@ class UnirseAQuiniela extends Component {
     header: null,
   };
 
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', () => this.props.navigation.goBack())
+  }
+ 
   buscarCodigo () {
-    alert("Aquí se debe buscar el código");
+    alert("Aquí se debe buscar el código. Generado Aleatorio: " + this.generaCodigo());
+    console.error (this.props.navigation);
+  }
+
+  generaCodigo () {
+    var caracteresePosibles = ['A','B','C','D','E','F','G','H','I','J',
+                               'K','L','M','N','O','P','Q','R','S','T',
+                               'U','V','W','X','Y','Z','0','1','2','3',
+                               '4','5','6','7','8','9'];    // Combinaciones posibles: 1.679.616
+    var codigo;
+    codigo = this.elementoAleatorio(caracteresePosibles);
+    codigo += this.elementoAleatorio(caracteresePosibles);
+    codigo += this.elementoAleatorio(caracteresePosibles);
+    codigo += this.elementoAleatorio(caracteresePosibles);
+    return codigo;
+  }
+
+  elementoAleatorio (array) {
+    var max = array.length;
+    var valorAleatorio = Math.floor (Math.random() * max);
+    return array[valorAleatorio];
   }
 
   cancelar (navigate) {
-    navigate('TusQuinielas');
+    //navigate('TusQuinielas');
+    this.props.navigation.goBack();
   }
 
   render() {
