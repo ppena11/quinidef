8cfeab7bafe22e3f633a7803c4cdaaec1bc5bcf4
Francisco Díaz <frandiaz+git@gmail.com>
2018-05-26 15:58:47 -0500

    Agregada variable para apuntar a proyecto firebase (true: productivo, false: pruebas)

--- app/index.js
+++ app/index.js
@@ -19,20 +19,29 @@ EStyleSheet.build({
 });
 
 const App = ({ dispatch, nav }) => <RootNavigator />;
-
-const mapStateToProps = state => ({
-  nav: state.nav
-});
-
+const mapStateToProps = state => ({ nav: state.nav });
 const AppWithNavigation = connect(mapStateToProps)(App);
+
+const entornoProductivoFirebase = true; // cambiar a false para apuntar a proyecto de pruebas
 
 export default class extends Component {
   componentWillMount() {
-    const firebaseConfig = {
-      apiKey: "AIzaSyBTNTx1cp-bZ3SquR9d6btC974MUnsPMb0",
-      authDomain: "react-native-firebase-20f8d.firebaseapp.com",
-      databaseURL: "https://react-native-firebase-20f8d.firebaseio.com"
-    };
+    let firebaseConfig;
+    if(entornoProductivoFirebase) { // Entorno Productivo
+      firebaseConfig = {
+        apiKey: "AIzaSyAjBk7uGmz4TzmBlEzi8VuHPC0GjVmXDuw",
+        authDomain: "futbol-y-quinielas.firebaseapp.com",
+        databaseURL: "https://futbol-y-quinielas.firebaseio.com",
+      };
+    }
+    else {
+      firebaseConfig = { // Entorno de Pruebas
+        apiKey: "AIzaSyBTNTx1cp-bZ3SquR9d6btC974MUnsPMb0",
+        authDomain: "react-native-firebase-20f8d.firebaseapp.com",
+        databaseURL: "https://react-native-firebase-20f8d.firebaseio.com",
+      };
+    }
+
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
     }
