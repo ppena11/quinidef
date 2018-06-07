43b00af5afbe7d96ff6a46eff24da09f13a66d93
Francisco Díaz <frandiaz+git@gmail.com>
2018-06-06 19:05:40 -0500

    Finalizado Cambio de Nombre

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from "react-native";
 import App from "./App";
 
-AppRegistry.registerComponent("Quiniela2", () => App);
+AppRegistry.registerComponent("futbolyquinielas", () => App);
